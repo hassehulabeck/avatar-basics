07bd30d1fd37600dd59e73fa4c79ff290cb39220
hassehulabeck <hansbudandersson@gmail.com>
2019-10-29 20:44:36 +0100

    Added avatar-constructor and first css

---
+++ index.js
@@ -0,0 +1,8 @@
+import * as avatar from './modules/avatar.mjs';
+let avatars = [];
+
+for (let i = 0; i < 5; i++) {
+    avatars.push(new avatar.Avatar);
+}
+
+// DOM
---
+++ modules/avatar.mjs
@@ -0,0 +1,14 @@
+var avatarTypes = ["gridy", "male", "female", "human", "bottts", "avataaars"];
+
+function Avatar(name, age) {
+    this.name = name;
+    this.age = age;
+    let slump = Math.floor(Math.random() * avatarTypes.length);
+    this.type = avatarTypes[slump];
+    this.image = `https://avatars.dicebear.com/v2/${this.type}/${this.name}-${this.age}`;
+}
+
+export {
+    avatarTypes,
+    Avatar
+};
