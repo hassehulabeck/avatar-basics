c81c9c026c271a2043b6f7df74493161ef7a8cfb
hassehulabeck <hansbudandersson@gmail.com>
2019-10-29 20:48:49 +0100

    Added first dom

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import * as avatar from './modules/avatar.mjs';
+import * as avatarModule from './modules/avatar.mjs';
 let avatars = [];
 
 for (let i = 0; i < 5; i++) {
-    avatars.push(new avatar.Avatar);
+    avatars.push(new avatarModule.Avatar);
 }
 
 // DOM
+document.addEventListener("DOMContentLoaded", () => {
+    var main = document.getElementsByTagName("main");
+
+    avatars.forEach((avatar) => {
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = `<img src="${avatar.image}">
+        <h1>${avatar.name}</h1>
+        <p>Ålder: ${avatar.age}</p>
+        `;
+        main[0].appendChild(newDiv);
+    })
+})
